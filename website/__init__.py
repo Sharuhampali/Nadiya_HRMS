f050ee6fcaff8fbd8150a0635f8d6fcff19284e1
Sharuhampali <hampalisharu@gmail.com>
2025-06-22 22:38:42 +0530

    fixed docker issue

--- website/__init__.py
+++ website/__init__.py
@@ -25,7 +25,7 @@ def create_app():
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     # app.config['SQLALCHEMY_DATABASE_URI'] = (
-    #     'postgresql://postgres:nadiya1@db.ltvqpcocsivhtnvkaagl.supabase.co:5432/postgres'
+    #     'postgresql://postgres:nadiya1@db.ltvqpcocsivhtnvkaagl.supabase.in:5432/postgres'
     # )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
