695e57a49f48555465888f32bed3a8dc7aae2e27
Sharuhampali <hampalisharu@gmail.com>
2025-06-27 03:12:02 +0530

    main fix

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 # main.py
 
-# import os
-# from website import create_app
-
-# app = create_app()
-
-# if __name__ == '__main__':
-#     port = int(os.environ.get('PORT', 8080))  # Use PORT=8080 from Cloud Run
-#     app.run(host='0.0.0.0', port=port)
-from website import create_app, db
-
+import os
+from website import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8080))  # Use PORT=8080 from Cloud Run
+    app.run(host='0.0.0.0', port=port)
+from website import create_app, db
+
+
+# app = create_app()
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
      
