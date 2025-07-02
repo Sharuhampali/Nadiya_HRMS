10d9f54788f32a82fdb5b3ed695f86500a323f18
Sharuhampali <hampalisharu@gmail.com>
2025-07-02 08:00:28 +0530

    logging

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# main.py
+# # main.py
 
-import os
-from website import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))  # Use PORT=8080 from Cloud Run
-    app.run(host='0.0.0.0', port=port)
-
+# import os
+# from website import create_app
 
 # app = create_app()
 
 # if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0', port=5000)
+#     port = int(os.environ.get('PORT', 8080))  # Use PORT=8080 from Cloud Run
+#     app.run(host='0.0.0.0', port=port)
+
+
+# # app = create_app()
+
+# # if __name__ == '__main__':
+# #     app.run(debug=True, host='0.0.0.0', port=5000)
+
+import os
+from website import create_app
+
+try:
+    app = create_app()
+except Exception as e:
+    print("App failed to start:", e)
+    raise e  # Ensure Cloud Run logs the actual error
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
