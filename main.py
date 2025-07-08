103e485ef357c80c65183bf58499260f94fd4a69
Sharuhampali <hampalisharu@gmail.com>
2025-07-08 17:38:29 +0530

    main

--- main.py
+++ main.py
@@ -3,14 +3,14 @@
 import os
 from website import create_app
 
-app = create_app()
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))  # Use PORT=8080 from Cloud Run
-    app.run(host='0.0.0.0', port=port)
-
-
 # app = create_app()
 
 # if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0', port=5000)
+#     port = int(os.environ.get('PORT', 8080))  # Use PORT=8080 from Cloud Run
+#     app.run(host='0.0.0.0', port=port)
+
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
